8498a386d7a3e36bb4a5e509c1dac0c851dceaf8
miyazaki.carp <miyazaki.carp@gmail.com>
2021-03-27 20:17:46 +0900

    Fix the main script to get articles to query with single article tag.

--- README.md
+++ README.md
@@ -31,7 +31,7 @@ Total Count of articles: 640054
 You can get tags with more than or equals to `count_threshold` followers. The result `.tsv` file will be in `tsv` directory. The `.tsv` file name can be changed by the `file_name` argument. 
 
 ```
-$ python get_tags --page_num=100 --count_threshold=30 --file_name=tags
+$ python get_tags.py --page_num=100 --count_threshold=30 --file_name=tags
 ```
 
 |Argument Name|Description|Default Value|
@@ -44,6 +44,20 @@ $ python get_tags --page_num=100 --count_threshold=30 --file_name=tags
 
 ## Get articles
 
-T.B.D.
+You can download Qiita article with the specified single tag. If you want to know the popular tag in Qiita, check the `tags/tags.tsv` directory in this project. To avoid duplication of downloading the same article, `output/output.tsv` will be used (this file will be created when you execute `main.py` for the first time). 
+
+|Argument Name|Description|Default Value|
+|:---|:---|:---|
+|tag|The tag attached to the articles. You can only one tag to select. This argument is required.|-|
+|page_num|Qiita API pagenation value.|10|
+|per_page|The number of articles in each page.|1|
+
+For example, if you execute the script with the following arguments, 
+
+```
+$ python main.py Python --page_num=10 --per_page=5
+```
+
+the maximum number of articles with tag `Python` are `10x5=50`. 
 
 ---
--- main.py
+++ main.py
@@ -8,14 +8,14 @@ import time
 import common
 
 
-PER_PAGE = 100
 OUT_PATH = "./output/output.tsv"
 
 
 def get_args():
     parser = argparse.ArgumentParser(description="Parser for get_tags.")
+    parser.add_argument("tag", help="You have to specify tag to select articles.")
     parser.add_argument("-p", "--page_num", type=int, default=10, help="The number of pages.")
-    parser.add_argument("-l", "--list_name", default="./tags/tags.tsv", help="The input file path of the tag list.")
+    parser.add_argument("-pp", "--per_page", type=int, default=1, help="The number of items in each page.")
     return parser.parse_args()
 
 
@@ -31,8 +31,17 @@ def check_output_exists():
 
 def main():
     args = get_args()
-    list_name = args.list_name
     page_num = args.page_num
+    per_page = args.per_page
+    selected_tag = args.tag
+    if page_num < 1:
+        page_num = 1
+    if page_num > 100:
+        page_num = 100
+    if per_page < 1:
+        per_page = 1
+    if per_page > 100:
+        per_page = 100
 
     h = common.get_header()
     url = "https://qiita.com/api/v2/items?page={}&per_page={}&query=tag%3A{}"
@@ -44,59 +53,53 @@ def main():
         print(df.head(5))
         already_counted_id_list = df["id"].values
 
-    error_flg = False
+    # create new output file if it does not exist.
+    if not os.path.exists(OUT_PATH):
+        with open(OUT_PATH, "w") as f:
+            f.write("id\ttags\n")
 
-    tag_list = get_tag_list(list_name)
-
+    # Qiita API call
     with open(OUT_PATH, "a") as f:
-        f.write("id\ttags\n")
+        print("----- TAG: {} -----".format(selected_tag))
+        for page in range(1, page_num + 1):
+            api_req = url.format(page, per_page, selected_tag)
+            print(api_req)
+            res = requests.get(api_req, headers=h)
+            data = res.text
 
-        for tag_index, tag in enumerate(tag_list):
-            print("----- [{}]: {} -----".format(tag_index+1, tag))
-
-            if error_flg:
-                print("API Error!")
+            if res.status_code != 200:
+                print("Status code: {}".format(res.status_code))
                 break
 
-            for page in range(1, page_num + 1):
-                api_req = url.format(page, PER_PAGE, tag)
-                res = requests.get(api_req, headers=h)
-                data = res.text
+            if data is None or data is '':
+                continue
+            
+            try:
+                d = json.loads(data)
+                for d_item in d:
+                    # ignoring already saved article
+                    article_id = d_item["id"]
+                    if article_id in already_counted_id_list:
+                        print("Skip article with id: {}".format(article_id))
+                        continue
 
-                if res.status_code != 200:
-                    print("Status code: {}".format(res.status_code))
-                    error_flg = True
-                    break
+                    # tag list -> tag concatenated by ","
+                    tags = d_item["tags"]
+                    tag_list = []
+                    for tag in tags:
+                        single_tag = tag["name"]
+                        tag_list.append(single_tag)
+                    tags_str = ",".join(tag_list)
+                    #print("id={}, tag_list={}".format(article_id, tags_str))
+                    f.write("{}\t{}\n".format(article_id, tags_str))
 
-                if data is None or data is '':
-                    continue
-                
-                try:
-                    d = json.loads(data)
-                    for d_item in d:
-                        # ignoring already saved article
-                        article_id = d_item["id"]
-                        if article_id in already_counted_id_list:
-                            print("Skip article with id: {}".format(article_id))
-                            continue
-
-                        # tag list -> tag concatenated by ","
-                        tags = d_item["tags"]
-                        tag_list = []
-                        for tag in tags:
-                            single_tag = tag["name"]
-                            tag_list.append(single_tag)
-                        tags_str = ",".join(tag_list)
-                        #print("id={}, tag_list={}".format(article_id, tags_str))
-                        f.write("{}\t{}\n".format(article_id, tags_str))
-
-                        # body
-                        body = d_item["body"]
-                        with open("./docs/{}.md".format(article_id), "w") as doc_f:
-                            doc_f.write(body)
-                
-                except Exception:
-                    pass
+                    # body
+                    body = d_item["body"]
+                    with open("./docs/{}.md".format(article_id), "w") as doc_f:
+                        doc_f.write(body)
+            
+            except Exception:
+                pass
 
 
 if __name__ == '__main__':
